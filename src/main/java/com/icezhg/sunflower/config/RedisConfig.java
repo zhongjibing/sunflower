6f03d8b003befb231622311802d6a6eb100acf53
zhongjibing <zhongjibing@163.com>
2024-06-20 17:03:25 +0800

    add bean: cacheManager

--- src/main/java/com/icezhg/sunflower/config/RedisConfig.java
+++ src/main/java/com/icezhg/sunflower/config/RedisConfig.java
@@ -2,12 +2,19 @@ package com.icezhg.sunflower.config;
 
 
 import com.icezhg.sunflower.util.ObjectMapperFactory;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @Configuration(proxyBeanMethods = false)
 public class RedisConfig {
@@ -27,6 +34,20 @@ public class RedisConfig {
     @Bean
     public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
         return Jackson2JsonRedisSerializerHolder.INSTANCE;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        return RedisCacheManager
+                .builder(redisConnectionFactory)
+                .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig()
+                        .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer.UTF_8))
+                        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(Jackson2JsonRedisSerializerHolder.INSTANCE))
+                        .disableCachingNullValues()
+                        .prefixCacheNameWith("sunflower:")
+                        .entryTtl(Duration.ofMinutes(30L))
+                )
+                .build();
     }
 
     private static class Jackson2JsonRedisSerializerHolder {
